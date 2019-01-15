9200e970cca416405f6bcc730aeef41c57793d9e
Jack <jack@developerjack.com>
2019-01-15 14:46:21 +0800

    fixes 0x01 and 0x02 delimiter bugs in #1 for both encoding and decoding

--- test/RFC8188Test.php
+++ test/RFC8188Test.php
@@ -9,18 +9,31 @@ use Base64Url\Base64Url as b64;
 
 final class RFC8188Test extends TestCase
 {
+
+    public function testDecryptRFC8188Example31(): void
+    {
+        $encoded = b64::decode("I1BsxtFttlv3u_Oo94xnmwAAEAAA-NAVub2qFgBEuQKRapoZu-IxkIva3MEB1PD-ly8Thjg");
+        
+        $decoded = RFC8188::rfc8188_decode(
+            $encoded, // data to decode 
+            [b64::decode("yqdlZ-tYemfogSmv7Ws5PQ")] // Keys
+        );
+
+        $this->assertEquals("I am the walrus", $decoded);
+    }
+
     public function testIAmTheWalrus(): void
     {
         $keys = [
             b64::decode('yqdlZ-tYemfogSmv7Ws5PQ'),
         ];
-        $message = "I Am the walrus";
+        $message = "I am the walrus";
 
         $encoded = RFC8188::rfc8188_encode(
             $message, // plaintext
             b64::decode("yqdlZ-tYemfogSmv7Ws5PQ"), // encryption key
             null,   // key ID
-            4096    // record size.
+            132    // record size.
         );
         $decoded = RFC8188::rfc8188_decode(
             $encoded, // data to decode 
